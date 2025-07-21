857e1d38eaef3b25693791debee2efcba674a96c
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2025-07-21 16:03:56 +0530

    resolved errors

--- client/react-estate-ui/src/lib/apiRequest.js
+++ client/react-estate-ui/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "http://localhost:8800/api",
   withCredentials: true,
 });
 
