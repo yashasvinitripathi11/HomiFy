0e8f82bb178a00b8b2b2ceb2b76e9d69fd7bb73b
Yashasvini Tripathi <yashasvinitripathi@gmail.com>
2025-07-21 23:33:51 +0530

    Fix: Minor changes

--- api/app.js
+++ api/app.js
@@ -5,12 +5,13 @@ import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.use(cors(
-  "*"
- ));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +21,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 
-app.listen(8800, () => {
-  console.log("Server is running!");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
