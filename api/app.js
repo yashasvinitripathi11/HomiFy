9a90b413cc557f239ee2dda5f1d4fe151639c999
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2025-07-21 14:41:28 +0530

    fix env

--- api/app.js
+++ api/app.js
@@ -11,10 +11,18 @@ import userRoute from "./routes/user.route.js";
 const app = express();
 
 //  app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+const allowedOrigins = [
+  'https://homi-fy.vercel.app',
+  'https://homi-fy-eas9.vercel.app',
+];
+
 app.use(cors({
-  origin: (origin, callback) => {
-    console.log("Incoming request origin:", origin);
-    callback(null, true);
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
   },
   credentials: true,
 }));
