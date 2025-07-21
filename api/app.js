56cde38d64c8636e9bbfb4d17611fd65a4bed56b
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2025-07-21 16:10:48 +0530

    :wq

--- api/app.js
+++ api/app.js
@@ -5,23 +5,12 @@ import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
+// import chatRoute from "./routes/chat.route.js";
+// import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-// Define allowed origins
-const allowedOrigins = ["https://homi-fy-eas9.vercel.app"];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
-
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -29,6 +18,9 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
+// app.use("/api/chats", chatRoute);
+// app.use("/api/messages", messageRoute);
 
-
-export default app;
+app.listen(8800, () => {
+  console.log("Server is running!");
+});
--- api/index.js
+++
@@ -1,4 +0,0 @@
-import app from "../app.js";
-import { createServer } from "@vercel/node";
-
-export default app;
