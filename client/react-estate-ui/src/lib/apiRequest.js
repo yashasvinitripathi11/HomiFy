38635c26102db4e10465ca7db66c1f85e5aeab40
Yashasvini Tripathi <yashasvinitripathi@gmail.com>
2025-07-21 23:36:12 +0530

    Minor changes 2

--- client/react-estate-ui/src/lib/apiRequest.js
+++ client/react-estate-ui/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api",
+  baseURL: "https://homify-api-3wrq.onrender.com/api",
   withCredentials: true,
 });
 
