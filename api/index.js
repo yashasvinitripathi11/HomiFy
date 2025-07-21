1f28068113f2c0cbd17349128ea05ff9f35b926a
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2025-07-21 15:17:27 +0530

    fix env

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import authRoute from "./routes/auth.route.js";
-import postRoute from "./routes/post.route.js";
-import testRoute from "./routes/test.route.js";
-import userRoute from "./routes/user.route.js";
-//import chatRoute from "./routes/chat.route.js";
-//import messageRoute from "./routes/message.route.js";
+import authRoute from "../routes/auth.route.js";
+import postRoute from "../routes/post.route.js";
+import testRoute from "../routes/test.route.js";
+import userRoute from "../routes/user.route.js";
+// import chatRoute from "../routes/chat.route.js";
+// import messageRoute from "../routes/message.route.js";
 
 const app = express();
 
-//  app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 const allowedOrigins = [process.env.FRONTEND_URL];
 
 app.use(cors({
@@ -18,7 +17,7 @@ app.use(cors({
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
@@ -31,9 +30,9 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
-//app.use("/api/chats", chatRoute);
-//app.use("/api/messages", messageRoute);
+// app.use("/api/chats", chatRoute);
+// app.use("/api/messages", messageRoute);
 
-app.listen(8800, () => {
-  console.log("Server is running!");
-});
+
+
+export default app;
---
+++ api/server.js
@@ -0,0 +1,5 @@
+import app from "./index.js";
+
+export default function handler(req, res) {
+  return app(req, res);
+}
